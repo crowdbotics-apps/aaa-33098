f706b7c9025296aedd7095a3996879276e7a912a
Crowdbotics <team@crowdbotics.com>
2022-01-23 12:35:20 +0000

    Update components

---
+++ screens/Login(30:104)3271726_30_104/index.js
@@ -0,0 +1,62 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_30_105} />
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("112%") },
+  View_30_105: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("113%"),
+    minHeight: hp("113%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(255, 255, 255, 1)"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
